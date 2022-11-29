557737844f73d1572f5cacd84ea92de897329557
giane-09 <gianelaamv@gmail.com>
2022-11-29 17:52:22 -0500

    avancevideo15_16

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class HomePage extends StatelessWidget {
+  //uri
+  //url
+  getDataPokemon() async{
+    Uri _uri = Uri.parse("https://raw.githubusercontent.com/Biuni/PokemonGo-Pokedex/master/pokedex.json");
+    http.Response response = await http.get(_uri);
+    print(response.statusCode);
+    print(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+         child: Text("Data"),
+         onPressed: (){
+           getDataPokemon();
+         }
+        ),
+      ),
+    );
+  }
+}
